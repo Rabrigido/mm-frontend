017a5cb30c0c855a0922fbc17a22c0ff344e529c
Rabrigido <cristhian.rabi@alumnos.ucn.cl>
2025-09-04 15:33:53 -0400

    fix: update API base URL for development environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiBase: 'http://localhost:3000' // ajusta si tu server corre en otro host/puerto
+  //apiBase: 'http://localhost:3000' // ajusta si tu server corre en otro host/puerto
+  apiBase: 'http://138.197.238.93:3000'
 };
+//138.197.238.93
