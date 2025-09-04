04be8912f2d365dba074a550af683243bbbeb889
Rabrigido <cristhian.rabi@alumnos.ucn.cl>
2025-09-04 17:29:24 -0400

    fix: update API base URL to use secure HTTPS endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   //apiBase: 'http://localhost:3000' // ajusta si tu server corre en otro host/puerto
-  apiBase: 'http://138.197.238.93:3000'
+  apiBase: 'https://emefjs.duckdns.org'
 };
 //138.197.238.93
