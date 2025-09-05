d721390ff024e664301413ff6022e762a322ed08
Rabrigido <cristhian.rabi@alumnos.ucn.cl>
2025-09-04 23:09:06 -0400

    fix: add client hydration provider to app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withHashLocation()), // 👈 activa /#/
-    provideHttpClient(withFetch()),            // HttpClient (standalone)
-  ],
+    provideRouter(routes, withHashLocation()),
+    provideClientHydration(),
+    provideHttpClient(withFetch())
+  ]
 };
