a17cf9127043f02f9fa6bcf73d1e0b1ea272b2f0
Rabrigido <cristhian.rabi@alumnos.ucn.cl>
2025-09-05 01:09:39 -0400

    fix: update API base URL formatting in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  //apiBase: 'http://localhost:3000' // ajusta si tu server corre en otro host/puerto
-  apiBase: 'https://emefjs.duckdns.org'
+ //apiBase: 'http://localhost:3000' // ajusta si tu server corre en otro host/puerto
+ apiBase: 'https://emefjs.duckdns.org'
 };
-//138.197.238.93
+
